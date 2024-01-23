a008a9d0e2e1ce1e0aa4d985c253e4a65f717804
Maxim <lyskomax2@mail.ru>
2024-01-24 01:14:59 +0300

    Add Hometask4

---
+++ Hometask4/Program.cs
@@ -0,0 +1,117 @@
+﻿// Задача 4*(не обязательная): Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива. Под удалением понимается создание нового двумерного массива без строки и столбца.
+
+// Функция - создание двумерного массива из случайных чисел
+int[,] CreateTwoDimensionalArray(int n)
+{
+    Random rnd = new Random();
+    int[,] array = new int[n, n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+// Функция - вывод на экран двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"Строка массива {i}: ");
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Функция - поиск значения минимального элемента двумерного массива
+int FindMinElementArray(int[,] array)
+{
+    int minElement = array[0, 0];
+    int minRow = 0;
+    int minCol = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < minElement)
+            {
+                minElement = array[i, j];
+                minRow = i;
+                minCol = j;
+            }
+        }
+    }
+    return minElement;
+}
+
+// Функция - вывод массива без строк и столбцов с наименьшим значением элемента
+int[,] PrintArrayWithoutMin(int[,] array, int row, int col)
+{
+    int newRowCount = array.GetLength(0) - 1;
+    int newColCount = array.GetLength(1) - 1;
+    int[,] newArray = new int[newRowCount, newColCount];
+
+    for (int i = 0, newRow = 0; i < array.GetLength(0); i++)
+    {
+        if (i == row)
+        {
+            continue;
+        }
+            
+        for (int j = 0, newCol = 0; j < array.GetLength(1); j++)
+        {
+            if (j == col)
+            {
+                continue;
+            }
+            newArray[newRow,newCol] = array[i,j];
+            newCol++;
+        }
+        newRow++;
+    }
+    return newArray;
+}
+
+// Ввод данных
+Console.Clear();
+bool workProgramm = true;
+
+while (workProgramm)
+{
+    Console.Write("Введите число больше 1: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if (number > 1)
+    {
+        int[,] array = CreateTwoDimensionalArray(number);
+        PrintArray(array);
+        int minElement = array[0, 0];
+        int minRow = 0;
+        int minCol = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] < minElement)
+                {
+                    minElement = array[i, j];
+                    minRow = i;
+                    minCol = j;
+                }
+            }
+        }
+        int[,] delArray = PrintArrayWithoutMin(array, minRow, minCol);
+        Console.WriteLine("Новый массив с удаленными строкой и столбцом минимального элемента:");
+        PrintArray(delArray);
+        workProgramm = false;
+    }
+    else 
+    {
+        Console.WriteLine("Введено неверное число");
+    }
+}
